2fbd290a5bf3f3b98ee548cc4434a9f5f2615a12
tpriestley <tjpriestley@gmail.com>
2019-12-02 14:13:23 -0800

    all my views

--- router.js
+++ router.js
@@ -2,11 +2,13 @@ var HomeController = require('./Controllers/HomeController');
 
 var KevinShiController = require('./Controllers/KevinShiController');
 
+var TristanPController = require('./Controllers/TristanPController');
+
 // Routes
 module.exports = function(app){  
     // Main Routes
     app.get('/',      HomeController.Index);    
     app.post('/Home/ReceiveForm', HomeController.ReceiveForm);
-
     app.get('/KevinShi', KevinShiController.KevinShi);
+    app.get('/TristanP', TristanPController.TristanP);
 };
