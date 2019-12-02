4464e6a970a92039acbc7ebfcd7469f6e3da61cf
kpchee <49546795+kpchee@users.noreply.github.com>
2019-12-02 14:22:59 -0800

    fixed

--- router.js
+++ router.js
@@ -2,11 +2,8 @@ var HomeController = require('./Controllers/HomeController');
 
 var KevinShiController = require('./Controllers/KevinShiController');
 
-<<<<<<< HEAD
 var KevinCheeController = require('./Controllers/KevinCheeController')
-=======
 var TristanPController = require('./Controllers/TristanPController');
->>>>>>> 542332eeef14cde5f09e522e81c10bdf42f89054
 
 // Routes
 module.exports = function(app){
@@ -14,10 +11,7 @@ module.exports = function(app){
     app.get('/',      HomeController.Index);
     app.post('/Home/ReceiveForm', HomeController.ReceiveForm);
     app.get('/KevinShi', KevinShiController.KevinShi);
-<<<<<<< HEAD
     app.get('/KevinChee', KevinCheeController.KevinChee);
-=======
     app.get('/TristanP', TristanPController.TristanP);
     app.get('SharleneTan', SharleneTan.SharleneTan)
->>>>>>> 542332eeef14cde5f09e522e81c10bdf42f89054
 };
